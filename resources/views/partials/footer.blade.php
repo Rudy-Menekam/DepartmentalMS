6b04c3597ba195ee54b3d3cdf84d4b66ee58aac1
fenn-cs <fenn25.fn@gmail.com>
2019-08-22 14:37:58 +0100

    shared footer partial for pages
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,110 @@
+<footer class="edulab-footer-area ">
+    <div class="footer-top-area section_50">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3">
+                    <div class="single-footer">
+                        <div class="text-widget">
+                            <a href="index.html">
+                                <h2>Departmental<span>MS</span></h2>
+                            </a>
+                            <p>Departmental MS is an online platform to help teachers to manage their courses and engage their students and collaborators.</p>
+                        </div>
+                        <div class="footer-social-icon">
+                            <h3>Follow Us</h3>
+                            <ul class="footer-social">
+                                <li>
+                                    <a href="#" class="twitter">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="gp">
+                                        <i class="fa fa-google-plus"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="linkedin">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="single-footer">
+                        <h3>usefull links</h3>
+                        <ul class="top-links">
+                            <li><a href="#"><i class="fa fa-angle-right"></i>Getting started</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right"></i>Courses</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right"></i>policies</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right"></i>blog post</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right"></i>support</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!--<div class="col-md-3 col-sm-3">
+                    <div class="single-footer">
+                        <h3>gallery</h3>
+                        <div class="footer-gallery">
+                            <a href="#">
+                                <img src="img/footer-img-1.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-2.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-3.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-1.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-2.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-3.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-1.jpg" alt="Footer Gallery" />
+                            </a>
+                            <a href="#">
+                                <img src="img/footer-img-3.jpg" alt="Footer Gallery" />
+                            </a>
+                        </div>
+                        <a href="#" class="load-more">Load more <i class="fa fa-angle-double-right"></i></a>
+                    </div>
+                </div> -->
+                <div class="col-md-3 col-sm-3">
+                    <div class="single-footer">
+                        <h3>Keep In Touch</h3>
+                        <div class="footer-newsletter">
+                            <p>Keep in touch for any questions and updates</p>
+                            <form>
+                                <input name="email" placeholder="Email Address" type="email">
+                                <input class="button" name="subscribe" type="submit">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="edulab-copyright-area section_15">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="edulab-copyright-left">
+                        <p> Copyright © 2017 Departmental MS.</p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="edulab-copyright-right">
+                        <p>Built with <i class="fa fa-heart"></i> by<span>Menekam Rudy</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
